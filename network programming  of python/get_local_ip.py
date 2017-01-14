07504bef9053b915e7ba43d379f6b51e3c729ea3
l-duan <christian.badguy@126.com>
2017-01-14 19:12:15 +0800

    commit some design format scripts

---
+++ network programming  of python/get_local_ip.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# Python Network Programming Cookbook
+# This program is optimized for Python 2.7. It may run on amy
+# other Python version with/without modifications.
+
+import socket
+
+def print_machine_info():
+    host_name = socket.gethostname()
+    ip_address = socket.gethostbyname(host_name)
+    print 'Host name: %s' % host_name
+    print 'IP address: %s' % ip_address
+
+if __name__ == '__main__':
+    print_machine_info()
