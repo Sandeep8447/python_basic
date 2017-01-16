db37a357666bdcf7ec510efd52c8bb3a43b840ee
l-duan <christian.badguy@126.com>
2017-01-16 21:21:26 +0800

    commit some design format scripts

--- network programming  of python/find_service_name.py
+++ network programming  of python/find_service_name.py
@@ -2,3 +2,14 @@
 # Python Network Programming Cookbook
 # This program is optimized for Python 2.7. It may run on amy
 # other Python version with/without modifications.
+
+import socket
+
+def find_service_name():
+    protocol_name = 'tcp'
+    for port in [80, 25, 3306, 22, 21]:
+        print 'Port: %s => service name: %s' %(port, socket.getservbyport(port, protocol_name))
+    print 'Port: %s => service name: %s' % (53, socket.getservbyport(53, 'udp'))
+
+if __name__ == '__main__':
+    find_service_name()
