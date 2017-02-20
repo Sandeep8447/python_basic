4d60f21260815340f929b94bbfd6d2aa41f7a3a9
l-duan <christianbadguy@123.com>
2017-02-20 20:12:24 +0800

    rename

--- day5_03/client_test.py
+++ day5_03/client_test.py
@@ -2,32 +2,15 @@
 __auth__ = 'christian'
 
 import socket
-import sys
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ip_port = ('127.0.0.1', 8080)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ip_port)
 
-# Connect the socket to the port where the server is listening
-server_address = ('192.168.11.99', 9090)
-print >>sys.stderr, 'connecting to %s port %s' % server_address
-sock.connect(server_address)
-
-try:
-
-    # Send data
-    message = 'This is the message.  It will be repeated.'
-    print >>sys.stderr, 'sending "%s"' % message
-    sock.sendall(message)
-
-    # Look for the response
-    amount_received = 0
-    amount_expected = len(message)
-
-    while amount_received < amount_expected:
-        data = sock.recv(16)
-        amount_received += len(data)
-        print >>sys.stderr, 'received "%s"' % data
-
-finally:
-    print >>sys.stderr, 'closing socket'
-    sock.close()
+while True:
+    data = client.recv(1024)
+    print data
+    inp = raw_input('client: ')
+    client.send(inp)
+    if inp == 'exit':
+        break
