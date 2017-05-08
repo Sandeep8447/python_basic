9708ba0170d4460b67a47d565ab6643ccb1a812e
duanlian <christianbadguy@126.com>
2017-05-08 15:03:10 +0800

    modify the directory

--- day7/multiprocessing_demo/muti_process_communication.py
+++ day7/multiprocessing_demo/muti_process_communication.py
@@ -1,17 +1,35 @@
 # -*- coding:utf-8 -*-
 __auth__ = 'christian'
 
-from multiprocessing import Process, Queue
-
 # the communication between processes, multiprocess use the same queue.
+# if using the Q2 module, then different process use the different queue
 
-def f(q, n):
+from multiprocessing import Process, Queue
+import Queue as Q2
+
+def f(q,n):
     q.put([n, 'hello'])
+    print q.get()
+    print q.get()
 
-if __name__ == '__main___':
-    que = Queue()
+def f2(q2,n):
+    q2.put([n, 'world'])
+    print q2.get()
+    print q2.get()
+
+if __name__ == '__main__':
+    # you can find the result have five flag(s)
+    q = Q2.Queue()
+    q.put('flag')
     for i in range(5):
-        p = Process(target=f, args=(que,i))
+        p = Process(target=f, args=(q,i))
         p.start()
-    while True:
-        print que.get()
+
+    # you can find only one flag.
+    q2 = Queue()
+    q2.put('flag')
+    for j in range(5):
+        p2 = Process(target=f2, args=(q2,j))
+        p2.start()
+        # while True:
+        # print q.get()
