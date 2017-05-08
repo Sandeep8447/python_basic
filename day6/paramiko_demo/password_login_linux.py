122ebd2a998bff9c4d2bdb761663a2c6ee004125
duanlian <christianbadguy@126.com>
2017-05-08 14:17:09 +0800

    modify the directory

--- day6/paramiko_demo/password_login_linux.py
+++ day6/paramiko_demo/password_login_linux.py
@@ -5,7 +5,7 @@ import paramiko
 
 ssh = paramiko.SSHClient() # isinstance
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy()) # sign
-ssh.connect(hostname= '192.168.1.109', port=22, username='hrg', password='hrg123')
+ssh.connect(hostname='192.168.11.90', port=22, username='root', password='123456')
 stdin, stdout, stderr = ssh.exec_command('free -h')
 print stdout.read()
 ssh.close()
