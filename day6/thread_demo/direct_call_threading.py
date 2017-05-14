6d4db5e32756e2554eebae73ce3ac7e2df030b88
l-duan <christian.badguy@126.com>
2017-05-14 21:27:58 +0800

    commit some script of multiprocess communication type: lock

--- day6/thread_demo/direct_call_threading.py
+++ day6/thread_demo/direct_call_threading.py
@@ -7,10 +7,10 @@ import time
 
 
 def sayhi(num):  # 定义每个线程要运行的函数
-
     print("running on number:%s" % num)
+    time.sleep(1)
+    print ("task done", num)
 
-    time.sleep(3)
 
 
 if __name__ == '__main__':
@@ -33,16 +33,19 @@ if __name__ == '__main__':
 
 
     start_time = time.time()
-    t_obj = []
+    t_obj = [] # 存线程实例
     for i in range(50):
         t = threading.Thread(target=sayhi, args=('t-%s' % i, ))
+        # t.setDaemon(True) # 把当前线程设置为守护进程
         t.start()
-        t_obj.append(t)
+        t_obj.append(t) # 为了不阻塞后面的线程启动，不在这里使用join方法，先存放到一个列表里
 
-    for t in t_obj:
+    for t in t_obj: # 循环线程实例表，等待所有线程执行完毕
         t.join()
 
-    print ('----------------')
+    print ('--------all threads have finished.--------')
+    # print (threading.current_thread(),threading.active_count())
+
     print ('cost:', time.time() - start_time)
 
 
--- day6/thread_demo/inherit_call_threading.py
+++ day6/thread_demo/inherit_call_threading.py
@@ -6,19 +6,24 @@ import time
 
 
 class MyThread(threading.Thread):
-    def __init__(self, num):
+    def __init__(self, num, sleep_time):
         threading.Thread.__init__(self)
         self.num = num
+        self.sleep_time = sleep_time
 
     def run(self):  # 定义每个线程要运行的函数
-
         print("running on number:%s" % self.num)
-
-        time.sleep(3)
+        time.sleep(self.sleep_time)
+        print ('task %s done' % self.num)
 
 
 if __name__ == '__main__':
-    t1 = MyThread(1)
-    t2 = MyThread(2)
+    t1 = MyThread('t1', 2)
+    t2 = MyThread('t2', 4)
     t1.start()
     t2.start()
+
+    t1.join()
+    #t2.join()
+
+    print '---main thread---'
