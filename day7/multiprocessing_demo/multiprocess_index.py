333660c322d9faf8d7f9bca0d428abac2592c210
l-duan <christianbadguy@126.com>
2017-07-04 19:18:48 +0800

    python semaphore

--- day7/multiprocessing_demo/multiprocess_index.py
+++ day7/multiprocessing_demo/multiprocess_index.py
@@ -1,11 +1,21 @@
 # -*- coding:utf-8 -*-
 __auth__ = 'christian'
 
+# multiprocessing
+# every processing starts a threading.
+
 import multiprocessing
+import threading
 import time
+
+def thread_run():
+    print (threading._get_ident())
+
 def f(name):
     time.sleep(2)
-    print ('hello', name)
+    print 'hello', name
+    t = threading.Thread(target=thread_run,)
+    t.start()
 
 if __name__ == '__main__':
     for i in range(10):
