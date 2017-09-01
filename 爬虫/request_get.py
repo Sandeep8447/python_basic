eca3a8c6bb30e482a1005e496da95cafe9752a81
l-duan <christian.badguy@126.com>
2017-09-02 00:12:46 +0800

    requests get

---
+++ 爬虫/request_get.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+__auth__ = 'christian'
+
+import requests
+
+def getHtmlContent(url):
+    try:
+        r = requests.get(url,  timeout=30)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        return r.text
+    except:
+        return 'something wrong!!'
+
+
+if __name__ == '__main__':
+    url = 'https://httpbin.org'
+    result = getHtmlContent(url)
+    print result
