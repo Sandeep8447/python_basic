9f141940a03618b20877e0226157bdf9665cb934
l-duan <christian.badguy@126.com>
2017-09-02 21:54:29 +0800

    网络爬虫的几个实例

---
+++ 爬虫/get_ip_location.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+__auth__ = 'christian'
+
+
+import requests
+
+url = 'http://ip.cn/index.php?ip='
+try:
+    kv = {'user-agent': 'mozilla/5.0'}
+    r= requests.get(url + '8.8.8.8', headers=kv)
+    r.raise_for_status()
+    r.encoding = r.apparent_encoding
+    print r.content[-1000:]
+except:
+    print "爬取失败"
--- 爬虫/request_get.py
+++ 爬虫/request_get.py
@@ -5,7 +5,8 @@ import requests
 
 def getHtmlContent(url):
     try:
-        r = requests.get(url,  timeout=30)
+        kv = {'user-agent': 'mozilla/5.0'}  # modify the http header for the python-requests
+        r = requests.get(url, headers=kv, timeout=30)
         r.raise_for_status()
         print r.raise_for_status()
         r.encoding = r.apparent_encoding
@@ -15,6 +16,6 @@ def getHtmlContent(url):
 
 
 if __name__ == '__main__':
-    url = 'https://www.duanlian.tech'
+    url = 'http://httpbin.org'
     result = getHtmlContent(url)
     print result
